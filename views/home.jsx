0bd19d8539f4ef7880f5baf2f56dc68fcd800a73
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-07-09 11:33:15 -0700

    Adding image

--- views/home.jsx
+++ views/home.jsx
@@ -3,17 +3,22 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-             <a href="/places">
-             <button className="btn-primary">Places Page</button>
-            </a>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="./images/pancake.jpg" alt="Pancake"/>
+                </div>
+                 <div>
+                 Photo by <a href="AUTHOR_LINK">Chad Montano</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                  </div>
 
-      </Def>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
-  }
-  
+}
 
 module.exports = home
