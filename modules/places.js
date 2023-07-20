662765e4fd12a67e9d1c98a6cce99884aa2cf9f9
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-07-20 14:40:44 -0700

    Adding Bootstrap grid to index page

--- modules/places.js
+++ modules/places.js
@@ -4,11 +4,11 @@ module.exports = [{
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
+    pic: '/public/css/images/thairestaurant.jpg'
   }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
+    pic: '/public/css/images/catcafe.jpg'
   }]
