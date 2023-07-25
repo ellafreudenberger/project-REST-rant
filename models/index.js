7c5923637cd33ed2e365919336d47d2e95ad9d44
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-07-25 01:08:53 -0700

    helper method altering how data is displayed

--- models/index.js
+++ models/index.js
@@ -6,4 +6,4 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 })
 
-module.exports.Place = require('./places')
+module.exports.Place = require('./place')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,33 +1,43 @@
-const React = require('react')
-const Def = require('../default')
+const React = require('react');
+const Def = require('../default');
 
-function show (data) {
-    return (
-        <Def>
-          <main>
-            <h1>{ data.place.name }</h1>
-            <div classname="Rating Section"> 
-            <h2> Rating </h2>
-            <h3>Currently Unrated </h3>  
+function Show(data) {
+  return (
+    <Def>
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+            <h1>{data.place.name}</h1>
+            <div className="Rating Section">
+              <h2>Rating</h2>
+              <h3>Currently Unrated</h3>
             </div>
-            <div classname="Comments Section"> 
-            <h2> Comments </h2>
-            <h3> No comments yet!</h3>
+            <div className="Comments Section">
+              <h2>Comments</h2>
+              <h3>No comments yet!</h3>
             </div>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-            Edit
-            </a>     
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-            <button type="submit" className="btn btn-danger">
-            Delete
-            </button>
-            </form> 
-          </main>
-        </Def>
-    )
-} 
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+              Edit
+            </a>
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+          </div>
+        </div>
+      </main>
+    </Def>
+  );
+}
 
-
-
-module.exports = show
-
+module.exports = Show;
