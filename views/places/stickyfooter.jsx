ffa3df4435dc706226c02f70f7f10fade2986a9c
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-07-24 23:27:25 -0700

    sticky footer

---
+++ views/places/stickyfooter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css'; 
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <footer className="sticky-footer">
+        <p>This app finds the coolest places!</p>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
