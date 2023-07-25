8efbb3e3a2e1ff95b6339709bbafa26b8b52ec57
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-07-24 23:05:35 -0700

    edit or delete the data for the place

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ require('./index.js'); // Replace 'your-main-file.js' with the actual entry poin
 // Modules and Globals
 require('dotenv').config()
 const express = require('express')
-const app = express()
+const methodOverride = require('method-override')
+
 
 // ECMAScript Modules to enable support for modern JavaScript syntax, including JSX in Node.js
 require = require("esm")(module);
@@ -16,6 +17,9 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 
 // Body Parser
 app.use(express.urlencoded({ extended: true }))
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,33 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
+            <div classname="Rating Section"> 
+            <h2> Rating </h2>
+            <h3>Currently Unrated </h3>  
+            </div>
+            <div classname="Comments Section"> 
+            <h2> Comments </h2>
+            <h3> No comments yet!</h3>
+            </div>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+            Edit
+            </a>     
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+            <button type="submit" className="btn btn-danger">
+            Delete
+            </button>
+            </form> 
           </main>
         </Def>
     )
-}
+} 
+
+
 
 module.exports = show
+
